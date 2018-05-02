8c3b47c21627a8f663d2e324a62ca84325b0d0af
hankersyan <hankersyan@msn.com>
2018-05-02 16:17:33 +0800

    bug of thread

--- src/ios/CDVSoftKeyboard.m
+++ src/ios/CDVSoftKeyboard.m
@@ -6,12 +6,7 @@
 @implementation CDVSoftKeyboard
 
 - (void)hide:(CDVInvokedUrlCommand*)command {
-	[self.commandDelegate runInBackground:^{
-		[cordova.view endEditing:YES];
-	    
-	    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-	    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-	}];
+    [self.webView endEditing:YES];
 }
 
 @end
